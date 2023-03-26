d9db01843270abac13051f850ba8caa9d8b594e0
Mikhail <mikhail86@icloud.com>
2023-03-26 14:16:33 +0100

    009.1 - updated the nav stylin.

--- javascript/index.js
+++ javascript/index.js
@@ -5,18 +5,6 @@ const navLinks = document.getElementById("nav-links");
 const toggleNav = () => {
   menuBtn.classList.toggle("open");
   navLinks.classList.toggle("active");
-
-  // animateHamburger();
 };
-
-// const animateHamburger = () => {
-//   if (!menuOpen) {
-//     menuBtn.classList.add("open");
-//     menuOpen = true;
-//   } else {
-//     menuBtn.classList.remove("open");
-//     menuOpen = false;
-//   }
-// };
 
 menuBtn.addEventListener("click", toggleNav);
